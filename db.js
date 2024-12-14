3b31f8d9db5ab0780f3df78ab1c7007d945fcc6c
DZed <idoy0utubebtw@gmail.com>
2024-12-15 01:42:42 +0800

    faker

--- db.js
+++
@@ -1,3 +0,0 @@
-const todos = ["ger", "hool"];
-
-module.exports = todos;
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const http = require("http");
-const todos = require("./db");
 const moment = require("moment");
+const { faker } = require('@faker-js/faker');
+const randomName = faker.person.fullName();
+function createRandomUser() {
+    return {
+      _id: faker.string.uuid(),
+      avatar: faker.image.avatar(),
+      birthday: faker.date.birthdate(),
+      email: faker.internet.email(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      sex: faker.person.sexType(),
+      subscriptionTier: faker.helpers.arrayElement(['free', 'basic', 'business']),
+    };
+  }
+  
 
-function test(req, res) {
+http.createServer((req, res) => {
+    const fakeUser = createRandomUser
     const birthday = "2006-12-15";
     const formattedDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-res.writeHead(200, {"content-type": "text/html"});
-res.end(formattedDate.toString());
-}
-
-http.createServer(test).listen(8080);
+    res.writeHead(200, {"content-type": "application/json"});
+    res.end(JSON.stringify(fakeUser));
+})
+.listen(8080);
