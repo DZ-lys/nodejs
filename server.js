f7eea7845c49723233282bcb71aa6632aee0d1ac
DZed <idoy0utubebtw@gmail.com>
2024-12-15 01:14:03 +0800

    moment date

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require("http");
+const todos = require("./db");
+const moment = require("moment");
 
 function test(req, res) {
+    const birthday = "2006-12-15";
+    const formattedDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 res.writeHead(200, {"content-type": "text/html"});
-res.end("bfsad");
+res.end(formattedDate.toString());
 }
 
 http.createServer(test).listen(8080);
